c273b347a19100725007beee3fa5a10e0faa203d
Shosa <kishosa@me.com>
2025-10-21 01:23:41 +0200

    netlify ready

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function OfflinePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-primary to-secondary flex items-center justify-center p-4">
